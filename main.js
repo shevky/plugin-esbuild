d31083f1747d5e07da6f3f167165be5f8c3dfc6b
Fatih Tatoğlu <fatihtatoglu@gmail.com>
2026-02-24 00:03:23 +0300

    update package fpr security

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { io, exec, plugin } from "@shevky/base";
 
 const PLUGIN_NAME = "shevky-esbuild";
-const PLUGIN_VERSION = "0.0.2";
+const PLUGIN_VERSION = "0.0.3";
 const PLUGIN_ROOT = import.meta.dirname ?? process.cwd();
 
 /** @type {import("@shevky/base").PluginHooks} */
